d4ceca86016ad5b5ccedc77e0d4735f6b99181e2
黄津明 <a2970539138@163.com>
2025-09-09 21:53:01 +0800

    11

---
+++ kernel/printk.h
@@ -0,0 +1,11 @@
+struct position
+{
+    int XResolution;
+    int YResolution;
+    int XPosition;
+    int YPosition;
+    int XCharSize;
+    int YCharSize;
+    unsigned int * FB_addr;
+    unsigned long FB_length;
+}Pos;
