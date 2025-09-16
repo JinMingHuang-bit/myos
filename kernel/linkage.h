9e10d9927f952d730ae10373b53fb4a14da489a3
@j29-huang <Jinming2.Huang@live.uwe.ac.uk>
2025-09-16 19:58:05 +0800

    add link.h

---
+++ kernel/linkage.h
@@ -0,0 +1,19 @@
+#ifndef _LINKAGE_H_
+#define _LINKAGE_H_
+
+// may change to 64
+#define L1_CACHE_BYTES 32
+
+#define __cacheline_aligned __attribute__((__aligned__(L1_CACHE_BYTES)))
+#define SYMBOL_NAME(X) X
+//它接受一个参数 X，并将其转换成一个字符串常量。
+#define SYMBOL_NAME_STR(X) #X
+//它接受一个参数 X，并将其与冒号 : 连接起来，形成一个新的标记（Token）。
+#define SYMBOL_NAME_LABEL(X) X##:
+
+#define ENTRY(name) \
+    .globl SYMBOL_NAME(name); \
+    SYMBOL_NAME_LABEL(name)
+
+
+#endif
