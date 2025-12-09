fff6479ca0e8f621f4a07453fbf37b67b0093bb8
@j29-huang <Jinming2.Huang@live.uwe.ac.uk>
2025-12-09 21:29:06 +0800

    do trap.c and entry.S

--- kernel/trap.c
+++ kernel/trap.c
@@ -114,7 +114,215 @@ void do_coprocessor_segment_overrun(unsigned long rsp,unsigned long error_code)
 	while(1);
 }
 
+void do_invalid_TSS(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_invalid_TSS(10),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
 
+	if(error_code & 0x01)
+		color_printk(RED,BLACK,"The exception occurred during delivery of an event external to the program,such as an interrupt or an earlier exception.\n");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Refers to a gate descriptor in the IDT;\n");
+	else
+		color_printk(RED,BLACK,"Refers to a descriptor in the GDT or the current LDT;\n");
+
+	if((error_code & 0x02) == 0)
+		if(error_code & 0x04)
+			color_printk(RED,BLACK,"Refers to a segment or gate descriptor in the LDT;\n");
+		else
+			color_printk(RED,BLACK,"Refers to a descriptor in the current GDT;\n");
+
+	color_printk(RED,BLACK,"Segment Selector Index:%#010x\n",error_code & 0xfff8);
+
+	while(1);
+}
+
+/*
+
+*/
+
+void do_segment_not_present(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_segment_not_present(11),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+
+	if(error_code & 0x01)
+		color_printk(RED,BLACK,"The exception occurred during delivery of an event external to the program,such as an interrupt or an earlier exception.\n");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Refers to a gate descriptor in the IDT;\n");
+	else
+		color_printk(RED,BLACK,"Refers to a descriptor in the GDT or the current LDT;\n");
+
+	if((error_code & 0x02) == 0)
+		if(error_code & 0x04)
+			color_printk(RED,BLACK,"Refers to a segment or gate descriptor in the LDT;\n");
+		else
+			color_printk(RED,BLACK,"Refers to a descriptor in the current GDT;\n");
+
+	color_printk(RED,BLACK,"Segment Selector Index:%#010x\n",error_code & 0xfff8);
+
+	while(1);
+}
+
+/*
+
+*/
+
+void do_stack_segment_fault(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_stack_segment_fault(12),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+
+	if(error_code & 0x01)
+		color_printk(RED,BLACK,"The exception occurred during delivery of an event external to the program,such as an interrupt or an earlier exception.\n");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Refers to a gate descriptor in the IDT;\n");
+	else
+		color_printk(RED,BLACK,"Refers to a descriptor in the GDT or the current LDT;\n");
+
+	if((error_code & 0x02) == 0)
+		if(error_code & 0x04)
+			color_printk(RED,BLACK,"Refers to a segment or gate descriptor in the LDT;\n");
+		else
+			color_printk(RED,BLACK,"Refers to a descriptor in the current GDT;\n");
+
+	color_printk(RED,BLACK,"Segment Selector Index:%#010x\n",error_code & 0xfff8);
+
+	while(1);
+}
+
+/*
+
+*/
+
+void do_general_protection(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_general_protection(13),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+
+	if(error_code & 0x01)
+		color_printk(RED,BLACK,"The exception occurred during delivery of an event external to the program,such as an interrupt or an earlier exception.\n");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Refers to a gate descriptor in the IDT;\n");
+	else
+		color_printk(RED,BLACK,"Refers to a descriptor in the GDT or the current LDT;\n");
+
+	if((error_code & 0x02) == 0)
+		if(error_code & 0x04)
+			color_printk(RED,BLACK,"Refers to a segment or gate descriptor in the LDT;\n");
+		else
+			color_printk(RED,BLACK,"Refers to a descriptor in the current GDT;\n");
+
+	color_printk(RED,BLACK,"Segment Selector Index:%#010x\n",error_code & 0xfff8);
+
+	while(1);
+}
+
+/*
+
+*/
+
+void do_page_fault(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	unsigned long cr2 = 0;
+	//movq %%cr2, %0: Move the value of the CR2 register to the output operand. 
+	//CR2 register: In the x86-64 architecture, it is specifically used to store the linear address of a page fault.
+	__asm__	__volatile__("movq	%%cr2,	%0":"=r"(cr2)::"memory");
+
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_page_fault(14),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+
+	if(!(error_code & 0x01))
+		color_printk(RED,BLACK,"Page Not-Present,\t");
+
+	if(error_code & 0x02)
+		color_printk(RED,BLACK,"Write Cause Fault,\t");
+	else
+		color_printk(RED,BLACK,"Read Cause Fault,\t");
+
+	if(error_code & 0x04)
+		color_printk(RED,BLACK,"Fault in user(3)\t");
+	else
+		color_printk(RED,BLACK,"Fault in supervisor(0,1,2)\t");
+
+	if(error_code & 0x08)
+		color_printk(RED,BLACK,",Reserved Bit Cause Fault\t");
+
+	if(error_code & 0x10)
+		color_printk(RED,BLACK,",Instruction fetch Cause Fault");
+
+	color_printk(RED,BLACK,"\n");
+
+	color_printk(RED,BLACK,"CR2:%#018lx\n",cr2);
+
+	while(1);
+}
+
+
+void do_x87_FPU_error(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_x87_FPU_error(16),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+	while(1);
+}
+
+/*
+
+*/
+
+void do_alignment_check(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_alignment_check(17),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+	while(1);
+}
+
+/*
+
+*/
+
+void do_machine_check(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_machine_check(18),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+	while(1);
+}
+
+/*
+
+*/
+
+void do_SIMD_exception(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_SIMD_exception(19),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+	while(1);
+}
+
+/*
+
+*/
+
+void do_virtualization_exception(unsigned long rsp,unsigned long error_code)
+{
+	unsigned long * p = NULL;
+	p = (unsigned long *)(rsp + 0x98);
+	color_printk(RED,BLACK,"do_virtualization_exception(20),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",error_code , rsp , *p);
+	while(1);
+}
 void sys_vector_init()
 {
     // set_trap_gate(0,1,divide_error);
