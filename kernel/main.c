95e78874c01b3b3250b21306a5b32b4aa9850ef5
黄津明 <a2970539138@163.com>
2026-04-08 22:12:41 +0800

    change vs config

--- kernel/main.c
+++ kernel/main.c
@@ -53,12 +53,12 @@ void Start_Kernel(void)
         *((char *)addr+3)=(char)0x00;
         addr++;
     }
-    i=1;
+    i=2;
     color_printk(YELLOW,BLACK,"the number is:%d\n",i);
     color_printk(YELLOW,BLACK,"the address is:%p\n", &i);
     load_TR(8);
     //set_tss64(0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00);
-    set_tss64(0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,
+    set_tss64(_stack_start,_stack_start,_stack_start,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,
 0xffff800000007c00,0xffff800000007c00,0xffff800000007c00,0xffff800000007c00);
     sys_vector_init();
     // struct  Global_Memory_Descriptor memory_management_struct={{0},0};
