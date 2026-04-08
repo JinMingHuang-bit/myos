d47b3442d1bc7249a6b4847299cc8d10af8b1bf7
黄津明 <a2970539138@163.com>
2026-04-08 13:35:30 +0800

    update make and ignore

--- kernel/task.h
+++ kernel/task.h
@@ -17,7 +17,7 @@
 
 #define	USER_CS		(0x28)
 #define USER_DS		(0x30)
-
+#define PF_KTHREAD	(1 << 0)
 extern char _text;
 extern char _etext;
 extern char _data;
@@ -118,9 +118,20 @@ struct tss_struct
     unsigned short reserved3;
     unsigned short iomapbaseaddr;
 }__attribute__((packed));//紧凑结构体,不进行内存对齐,确保布局正确性
-
-#define PF_KTHREAD	(1 << 0)
 struct mm_struct init_mm={0};
+struct thread_struct init_thread={
+    //init_task_union.stack + 元素个数 得到数组末尾的下一个位置（即 base + STACK_SIZE），也就是栈区域的最高地址 + 1,
+    //这样第一次执行 push 时，RSP 先减 8,正好指向第一个元素的地址.
+    .rsp0=(unsigned long)(init_task_union.stack + STACK_SIZE / sizeof(unsigned long)),
+    .rsp=(unsigned long)(init_task_union.stack + STACK_SIZE / sizeof(unsigned long)), //内核栈顶地址
+    //设置为 KERNEL_DS（内核数据段选择子），因为 idle 进程始终运行在内核态
+    .fs=KERNEL_DS,
+    .gs=KERNEL_DS,
+    .cr2=0,
+    .trap_nr=0,
+    .error_code=0
+};
+
 #define INIT_TASK(tsk)  \
 {   \
     .state = TASK_UNINTERRUPTIBLE,      \
@@ -152,18 +163,6 @@ struct mm_struct init_mm={0};
 }
 union task_union init_task_union __attribute__((__section__(".data.init_task"))) = {INIT_TASK(init_task_union.task)};
 struct task_struct *init_task[NR_CPUS] ={&init_task_union.task,0};
-struct thread_struct init_thread={
-    //init_task_union.stack + 元素个数 得到数组末尾的下一个位置（即 base + STACK_SIZE），也就是栈区域的最高地址 + 1,
-    //这样第一次执行 push 时，RSP 先减 8,正好指向第一个元素的地址.
-    .rsp0=(unsigned long)(init_task_union.stack + STACK_SIZE / sizeof(unsigned long)),
-    .rsp=(unsigned long)(init_task_union.stack + STACK_SIZE / sizeof(unsigned long)), //内核栈顶地址
-    //设置为 KERNEL_DS（内核数据段选择子），因为 idle 进程始终运行在内核态
-    .fs=KERNEL_DS,
-    .gs=KERNEL_DS,
-    .cr2=0,
-    .trap_nr=0,
-    .error_code=0
-};
 
 struct tss_struct init_tss[NR_CPUS]={[0 ... NR_CPUS-1]=INIT_TSS};
 
